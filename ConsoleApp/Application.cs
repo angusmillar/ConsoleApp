5f02461f2acfdb8bff1efb39229e50ae5e37f69f
Angus Millar <angusbmillar@gmail.com>
2024-03-23 14:24:26 +1000

    Initial Commit

---
+++ ConsoleApp/Application.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ConsoleApp;
+
+public class Application(
+    ILogger<Application> logger, 
+    IOptions<ApplicationConfiguration> appConfig)
+{
+    public void Run()
+    {
+        logger.LogInformation("Running: {ApplicationName}", appConfig.Value.ApplicationName);
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+    
+IServiceCollection services = new ServiceCollection();
+
+services.AddLogging();
+
+//Configuration
+services.AddOptions<ApplicationConfiguration>().Bind(configuration.GetSection(ApplicationConfiguration.SectionName));
+
+//Add Services
+services.AddSingleton<Application>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+serviceProvider.GetRequiredService<ILoggerFactory>()
+    .AddSerilog();
+
+var application = serviceProvider.GetRequiredService<Application>();
+
+application.Run();
+
+
